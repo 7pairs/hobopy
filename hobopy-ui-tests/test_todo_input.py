0f0f1e51616c92a02a7c986b6a9b930bff99998d
HASEBA Junya <7pairs@gmail.com>
2019-08-29 14:48:18 +0900

    Update with the contents of Chapter 10 Section 6

--- hobopy-ui-tests/test_todo_input.py
+++ hobopy-ui-tests/test_todo_input.py
@@ -1,31 +1,30 @@
-from selene.api import *
+from pages.todo_list_page import TodoListPage
 
 # ToDo新規登録のテストシナリオ
-class TestTodoInputSelene:
+class TestTodoInputPageobj:
 
     def test_ToDoを一件登録する(self, driver):
+
+        param_title = "CATCH the GLORY"
+        param_memo = "新時代、熱狂しろ！"
+        param_priority = "低"
+
+        expected_title = "CATCH the GLORY"
+
+        todo_list_page = TodoListPage(driver)
+
         # 画面を開く
-        driver.get('http://127.0.0.1:8002/index.html')
+        todo_list_page.open_page()
 
         # 登録ボタンをタップする
-        driver.s("#new-todo").click()
-        driver.s(".modal-title").should(be.visible)
+        todo_input_modal = todo_list_page.click_register()
 
         # 各入力欄に値を入力する
-        driver.s("#modal-todo-title").set_value("CATCH the GLORY")
-        driver.s("#modal-todo-memo").set_value("新時代、熱狂しろ！")
-        driver.s("#modal-todo-priority").click()
-        driver.s("#modal-todo-priority :nth-child(3)").click()
+        todo_input_modal.input_param(
+            param_title, param_memo, param_priority)
 
         # 登録ボタンをタップする
-        driver.s("#register-button").click()
+        todo_input_modal.click_register()
 
         # 入力した内容が表示されていることを確認
-        driver.s("#todo-list").should(be.visible)
-        assert_flg = False
-        todo_elements = driver.ss("#todo-list tr")
-        for todo_element in todo_elements:
-            if todo_element.s("td:nth-of-type(2)")\
-                    .text.find("CATCH the GLORY"):
-                assert_flg = True
-        assert assert_flg
+        assert todo_list_page.is_exist_title(expected_title)
